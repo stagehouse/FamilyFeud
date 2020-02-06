6c3ace40d5cccd20d224a62422836677b42c84be
jmusarra <johnm@stagehouselv.com>
2020-02-05 18:04:23 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from machine import Pin
+from uosc.client import Client
+import time
+import network
+print("main.py loaded")
+
+button1 = Pin(34, Pin.IN)
+button2 = Pin(35, Pin.IN)
+
+console = Client('192.168.1.162', 53000)
+nic = network.LAN(mdc = Pin(23), mdio = Pin(18), phy_type = network.PHY_LAN8720, phy_addr = 0, clock_mode = network.ETH_CLOCK_GPIO17_OUT)
+
+def networkConnect():
+    if not nic.isconnected():
+        nic.active(True)
+        nic.ifconfig(('192.168.1.254', '255.255.255.0','192.168.1.1', '192.168.1.1'))
+    print(nic.ifconfig())
+
+def player1():
+    console.send('/cue/1/start')
+    print("player1 sent")
+    time.sleep(5)
+
+def player2():
+    console.send('/cue/2/start')
+    print("player2 sent")
+    time.sleep(5)
+
+while True:
+    if not nic.isconnected():
+        networkConnect()
+    if nic.isconnected():
+        if button1.value() == 1:
+            player1()
+        if button2.value() == 1:
+            player2()
